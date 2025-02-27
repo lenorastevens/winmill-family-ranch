5ab5e96cf4a38a8065af17263b6ec14315bfdacd
Lenora Stevens <tlbs6513@gmail.com>
2025-02-26 18:35:58 -0600

    tweak tailwind for better mobile view

--- app/components/events/faq/index.tsx
+++ app/components/events/faq/index.tsx
@@ -17,11 +17,11 @@ const FAQItem: React.FC<FAQItemProps> = ({ question, answer }) => {
   };
 
   return (
-    <div className="faq-item">
-      <div className="question pl-4 py-2" onClick={toggleAnswer}>
+    <div className="faq-item pl-4 py-2">
+      <div className="question" onClick={toggleAnswer}>
         {question}{" "}{isOpen ? <ExpandLess /> : <ExpandMore />}
       </div>
-      {isOpen && <div className="answer py-2" onClick={toggleAnswer}>{answer}</div>}
+      {isOpen && <div className="answer mt-2" onClick={toggleAnswer}>{answer}</div>}
     </div>
   );
 };
@@ -30,10 +30,10 @@ const FAQ = ({ faqs }: { faqs: FAQType[] }) => {
   return (
     <div className='py-16 container mx-auto gap-x-10 grid md:grid-cols-2 '>
       <Info />
-      <div className="faq-container">
+      <div className="faq-container mx-4">
         {Array.isArray(faqs) && faqs.length > 0 ? (
           faqs.map((faq, index) => (
-            <div key={index} className="border-2 border-spacing-1 border-solid border-foreground py-4">
+            <div key={index} className="border-2 border-spacing-1 border-solid border-foreground rounded-lg mb-3 py-4">
             <FAQItem  question={faq.question} answer={faq.answer} />
             </div>
         ))
--- app/components/events/faq/info.tsx
+++ app/components/events/faq/info.tsx
@@ -2,7 +2,7 @@ export default function Info() {
   return (
     <div>
       <h1 className="text-5xl text-center mb-6">Frequently Asked Questions</h1>
-      <p className="mb-6">This year our reunion will be a work week.  We are not saying no fun, but we would like to accomplish big progress on big projects and that will require time.  We will mix work during parts of the day with fun activities and outings that all ages will enjoy.</p>
+      <p className="mb-6 mx-4">This year our reunion will be a work week.  We are not saying no fun, but we would like to accomplish big progress on big projects and that will require time.  We will mix work during parts of the day with fun activities and outings that all ages will enjoy.</p>
     </div>
   )
 }
